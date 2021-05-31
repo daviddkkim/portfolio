f41a4f69d7f52bafc30abf43c4705e4f8f53976a
David Kim <david.kim@richmond.edu>
2021-05-31 02:01:21 -0700

    optimize gif

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ const withOptimizedImages = require('next-optimized-images')
 
 module.exports = withOptimizedImages({
     handleImages: [ 'png', 'gif'],
+    optimizeImages: true,
+    gifsicle: {
+        interlaced: true,
+        optimizationLevel: 3,
+      },
     images: {
         domains: ['daviddkkim.com'],
       },
